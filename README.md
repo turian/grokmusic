ffa4702bb65a7fefe1164408004d86ef2fb063e4
Joseph Turian <turian@gmail.com>
2014-03-03 20:48:20 -0800

    more readme

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
+
+License: BSD
+
+Put your API key in a shell variable ECHO_NEST_API_KEY
+
 See also http://jmir.sourceforge.net/
+
+
+REQUIREMENTS
+------------
+
+* easy_install -U pyechonest
+
+* echoprint-codegen
+
+    brew install ffmpeg boost taglib
+    git checkout https://github.com/echonest/echoprint-codegen
+    cd echoprint-codegen/src/
+    make
---
+++ grokmusic.py
@@ -0,0 +1,20 @@
+
+# TODO: Read iTunes XML file
+
+
+import pyechonest.config as config
+import pyechonest.song as song
+
+config.CODEGEN_BINARY_OVERRIDE = os.path.abspath("echoprint-codegen/echoprint-codegen")
+
+# Put your API key in a shell variable ECHO_NEST_API_KEY, or put it here
+# config.ECHO_NEST_API_KEY='KEY HERE'
+
+def echoprint_bulk(files):
+    
+
+
+
+# Identify the machine
+
+# Identify the hash of each file
